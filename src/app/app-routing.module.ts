3b5478f408a8c379823ed0829175e1d0b868ba71
mutiaayuriz <Marizky2008@gmail.com>
2024-08-29 08:06:21 +0700

    update label name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, data: { title: 'Home' } },
+  { path: '', component: HomeComponent, data: { title: 'Beranda' } },
   { path: 'about', component: AboutComponent, data: { title: 'About' } },
 ];
 
